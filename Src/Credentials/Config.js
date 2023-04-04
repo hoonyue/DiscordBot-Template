1a71d2081214508b6d0d92750d53b35e5a757043
hoonyue <129539149+hoonyue@users.noreply.github.com>
2023-04-04 14:26:27 +0200

    Update Config.js

--- Src/Credentials/Config.js
+++ Src/Credentials/Config.js
@@ -1,6 +1,6 @@
 module.exports = {
     prefix: ["t", "1"],
-    developersIds: ["710851332692508744"],
-    botToken: "MTA5MTIwNTg4MTg1NDY4OTM0MA.GQVgvo.PF01IP8K-KRwt1f1EnXrtEKmBoEdzglUE12gQ4",
-    mongo: "mongodb+srv://churai:rYGCAWSMtgMWt6jH@tannies.tpuvzd2.mongodb.net/?retryWrites=true&w=majority"
+    developersIds: [""],
+    botToken: "",
+    mongo: ""
 }
